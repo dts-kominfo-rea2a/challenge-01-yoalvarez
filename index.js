0ba3ce3f6a7d2250af6820bda8722992993db479
yoalvarez <85821970+yoalvarez@users.noreply.github.com>
2022-05-28 21:56:00 +0700

    Update Penyelesaian Tugas
    -

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const colorUser1 = new Set([
+    "Yellow", "Pink", "White", "Purple"
+]);
+const educationUser1 = [
+    {name: "SD 01", city: "Jakarta", graduate: "2016"},
+    {name: "SMP 02", city: "Jakarta", graduate: "2019"},
+    {name: "SMA 03", city: "Tangerang"}
+];
+const restauranUser1 = new Set([
+ "Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento",  "Eggy", "Padang", "Tteok", "Sushi", "Sushi"
+]);
+const firstUser = {
+    name: "Monica",
+    gender: "Female",
+    age: "17",
+    email: "monica@dingdong.com",
+    favoriteColor: colorUser1,
+    isHavePet: "Yes",
+    education: educationUser1,
+    favoriteRestaurant: [...restauranUser1],
+};
+const colorUser2 = new Set([
+    "Blue", "Black", "Grey"
+]);
+const educationUser2 = [
+    {name: "SD 02", city: "Jakarta", graduate: "2010"},
+    {name: "SMP 03", city: "Bogor", graduate: "2013"},
+    {name: "SMA 01", city: "Surabaya", graduate: "2016"},
+    {name: "Universitas Maju", city: "Tangerang"}
+];
+const restauranUser2 = new Set([
+ "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu",  "Geprek", "Pancake", "Eggy"
+]);
+const secondUser = {
+    name: "Wendy",
+    gender: "Male",
+    age: "23",
+    email: "wendy@dingdong.com",
+    favoriteColor: colorUser2,
+    isHavePet: "No",
+    education: educationUser2,
+    favoriteRestaurant: [...restauranUser2],
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
