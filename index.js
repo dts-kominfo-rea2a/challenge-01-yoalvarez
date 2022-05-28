42cadb469cf784099837103106d0029753b2b569
yoalvarez <85821970+yoalvarez@users.noreply.github.com>
2022-05-28 22:16:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-let colorUser1 = new Set([
+const refColor1 = [
     "Yellow", "Pink", "White", "Purple"
-]);
+];
+const refResto1 = [
+    "Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"
+]; 
+let colorUser1 = new Set(refColor1);
+let restauranUser1 = new Set(refResto1);
+const refColor2 = [
+    "Blue", "Black", "Grey"
+];
+const refResto2 = [
+    "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
+];
+let colorUser2 = new Set(refColor2);
+let restauranUser2 = new Set(refResto2);
 const educationUser1 = [
     {name: "SD 01", city: "Jakarta", graduate: "2016"},
     {name: "SMP 02", city: "Jakarta", graduate: "2019"},
     {name: "SMA 03", city: "Tangerang"}
 ];
-let restauranUser1 = new Set([
- "Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento",  "Eggy", "Padang", "Tteok", "Sushi", "Sushi"
-]);
+const educationUser2 = [
+    {name: "SD 02", city: "Jakarta", graduate: "2010"},
+    {name: "SMP 03", city: "Bogor", graduate: "2013"},
+    {name: "SMA 01", city: "Surabaya", graduate: "2016"},
+    {name: "Universitas Maju", city: "Tangerang"}
+];
 const firstUser = {
     name: "Monica",
     gender: "Female",
@@ -22,18 +38,6 @@ const firstUser = {
     education: educationUser1,
     favoriteRestaurant: [...restauranUser1],
 };
-let colorUser2 = new Set([
-    "Blue", "Black", "Grey"
-]);
-const educationUser2 = [
-    {name: "SD 02", city: "Jakarta", graduate: "2010"},
-    {name: "SMP 03", city: "Bogor", graduate: "2013"},
-    {name: "SMA 01", city: "Surabaya", graduate: "2016"},
-    {name: "Universitas Maju", city: "Tangerang"}
-];
-let restauranUser2 = new Set([
- "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu",  "Geprek", "Pancake", "Eggy"
-]);
 const secondUser = {
     name: "Wendy",
     gender: "Male",
